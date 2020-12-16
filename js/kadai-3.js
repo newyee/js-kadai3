1c36422186978cacbea569710115e2142f9adcc6
newyee <takatelin01@icloud.com>
2020-12-17 01:08:09 +0900

    コード修正

--- js/kadai-3.js
+++ js/kadai-3.js
@@ -1,6 +1,5 @@
 'use strict';
-
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', () => {
   let id = 0;
   let todos = [];
   document.getElementById('task_add_button').addEventListener('click',function(){
@@ -18,29 +17,37 @@ window.addEventListener('DOMContentLoaded', function(){
     todos.push(todo);
     // console.log('todos',todos)
     // console.log('todos[0]',todos[id].task)
-
-    const tbody = document.getElementById('task_list')
+    const tbody = document.getElementById('task_list');
     // console.log(tbody)
-    const row = tbody.insertRow(-1)
+    const row = tbody.insertRow(-1);
     let tdId = row.insertCell();
     let tdComment = row.insertCell();
     let tdStateButton = row.insertCell();
     let tdDeleteButton = row.insertCell();
     let stateButton = document.createElement('button');
     let deleteButton = document.createElement('button');
-    stateButton.setAttribute('value', 'working');
-    deleteButton.setAttribute('value', '削除');
-    deleteButton.textContent = '削除';
+    // stateButton.setAttribute('value', 'working');
+    setStateButton(stateButton,todos);
+    setDeleteButton(deleteButton);
+    // deleteButton.setAttribute('value', '削除');
+    // deleteButton.textContent = '削除';
     tdId.textContent = id;
     tdComment.textContent = todos[id].task;
-    stateButton.textContent = todos[id].status;
+    // stateButton.textContent = todos[id].status;
     tdStateButton.appendChild(stateButton);
     tdDeleteButton.appendChild(deleteButton);
     // console.log(task_text);
     id += 1;
     taskElem.value = '';
   });
-  // function displayTodos(){
 
-  // }
+  const setStateButton = (stateButton, todos) => {
+    stateButton.setAttribute('value', 'working');
+    stateButton.textContent = todos[id].status;
+  }
+  const setDeleteButton = (deleteButton) => {
+    deleteButton.setAttribute('value', '削除');
+    deleteButton.textContent = '削除';
+  }
+
 });
