df0cf4794630aecd48a45f1654188c5db814a0c1
newyee <takatelin01@icloud.com>
2020-12-19 11:22:48 +0900

    コード修正

--- js/kadai-3.js
+++ js/kadai-3.js
@@ -2,18 +2,24 @@
 window.addEventListener('DOMContentLoaded', () => {
   let id = 0;
   let todos = [];
+  let todo = {};
   document.getElementById('task_add_button').addEventListener('click',function(){
     let taskElem = document.getElementById('task_text');
     let taskText = taskElem.value;
     if(!taskText){
       return
     }
-    let todo = {};
     todo = {
       task:taskText,
       status:'作業中'
     }
     todos.push(todo);
+    displayData();
+    id += 1;
+    taskElem.value = '';
+
+  });
+  const displayData = () => {
     const tbody = document.getElementById('task_list');
     const row = tbody.insertRow(-1);
     let tdId = row.insertCell();
@@ -22,19 +28,13 @@ window.addEventListener('DOMContentLoaded', () => {
     let tdDeleteButton = row.insertCell();
     let stateButton = document.createElement('button');
     let deleteButton = document.createElement('button');
-    // stateButton.setAttribute('value', 'working');
     setStateButton(stateButton,todos);
     setDeleteButton(deleteButton);
-    // deleteButton.setAttribute('value', '削除');
-    // deleteButton.textContent = '削除';
     tdId.textContent = id;
     tdComment.textContent = todos[id].task;
-    // stateButton.textContent = todos[id].status;
     tdStateButton.appendChild(stateButton);
     tdDeleteButton.appendChild(deleteButton);
-    id += 1;
-    taskElem.value = '';
-  });
+  }
 
   const setStateButton = (stateButton, todos) => {
     stateButton.setAttribute('value', 'working');
