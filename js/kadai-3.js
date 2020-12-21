2b74b2a1cf67252be024e61eb33d25895b4373bb
newyee <takatelin01@icloud.com>
2020-12-21 10:36:10 +0900

    コード修正

--- js/kadai-3.js
+++ js/kadai-3.js
@@ -1,17 +1,16 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
   let id = 0;
-  let todos = [];
-  let todo = {};
-  document.getElementById('task_add_button').addEventListener('click',function(){
-    let taskElem = document.getElementById('task_text');
-    let taskText = taskElem.value;
+  const todos = [];
+  document.getElementById('task_add_button').addEventListener('click', () => {
+    const taskElem = document.getElementById('task_text');
+    const taskText = taskElem.value;
     if(!taskText){
       return
     }
-    todo = {
-      task:taskText,
-      status:'作業中'
+    const todo = {
+      task: taskText,
+      status: '作業中'
     }
     todos.push(todo);
     displayData();
@@ -26,23 +25,25 @@ window.addEventListener('DOMContentLoaded', () => {
     let tdComment = row.insertCell();
     let tdStateButton = row.insertCell();
     let tdDeleteButton = row.insertCell();
-    let stateButton = document.createElement('button');
-    let deleteButton = document.createElement('button');
-    setStateButton(stateButton,todos);
-    setDeleteButton(deleteButton);
+    const stateButton = createStateButton();
+    const deleteButton = createDeleteButton();
     tdId.textContent = id;
     tdComment.textContent = todos[id].task;
     tdStateButton.appendChild(stateButton);
     tdDeleteButton.appendChild(deleteButton);
   }
 
-  const setStateButton = (stateButton, todos) => {
+  const createStateButton = () => {
+    const stateButton = document.createElement('button');
     stateButton.setAttribute('value', 'working');
     stateButton.textContent = todos[id].status;
+    return stateButton
   }
-  const setDeleteButton = (deleteButton) => {
+  const createDeleteButton = () => {
+    const deleteButton = document.createElement('button');
     deleteButton.setAttribute('value', '削除');
     deleteButton.textContent = '削除';
+    return deleteButton
   }
 
 });
